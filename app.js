49f1b49f870d487f61deb00adb7762e3a6fddb15
Sebastian Coceres <sebastian.coceres1@hotmail.com>
2021-06-14 20:41:01 +0200

    solucion en pantallas acostadas y agregue el marcador

--- app.js
+++ app.js
@@ -4,14 +4,19 @@ const rockButton = document.getElementById("Rock");
 const paperButton = document.getElementById("Paper");
 const scissorsButton = document.getElementById("Scissors");
 const endGame = document.getElementById("endGame");
+const scoreGame = document.getElementById("scoreGame");
 
 let choicePc;
 let choiceUser;
 
+let scoreUser = 0;
+let scorePc = 0;
+
+document.getElementById("scoreGame").innerHTML = scoreUser + " - " + scorePc;
 
 function userInteraction(){
     console.log("elija su opción");
-
+    
     rockButton.addEventListener("click", getSelectionUser);
     paperButton.addEventListener("click", getSelectionUser);
     scissorsButton.addEventListener("click", getSelectionUser);
@@ -43,28 +48,41 @@ function game(){
     if (choiceUser == "rock" && choicePc == "rock" || choiceUser == "paper" && choicePc == "paper" || choiceUser == "scissors" && choicePc == "scissors") {
 
         console.log("Tie");
+        
         document.getElementById("game").innerHTML = "Tie" + "<br /> 🤝";
+        document.getElementById("scoreGame").innerHTML = scoreUser + " - " + scorePc;
     
     } else if (choiceUser == "rock" && choicePc == "scissors" || choiceUser == "paper" && choicePc == "rock" || choiceUser == "scissors" && choicePc == "paper") {
     
         console.log("You Win!");
         document.getElementById("game").innerHTML = "You Win!" + "<br />🏆";
-    
+        scoreUser++;
+
+        document.getElementById("scoreGame").innerHTML = scoreUser + " - " + scorePc;
+
     } else if (choiceUser == "rock" && choicePc == "paper" || choiceUser == "paper" && choicePc == "scissors" || choiceUser == "scissors" && choicePc == "rock") {
     
         console.log("You Lose!");
         document.getElementById("game").innerHTML = "You Lose!" + "<br /> 😓";
+        scorePc++;
 
+        document.getElementById("scoreGame").innerHTML = scoreUser + " - " + scorePc;
     }
 
+
+    console.log("Pc: " + scorePc);
+    console.log("Your: " + scoreUser);
 }
 
 function endGameRegards() {
         console.log("Bye");
         console.log(" ");
-
+        document.getElementById("rightHand").innerHTML = choicesArrayIcons[0];
+        document.getElementById("leftHand").innerHTML = choicesArrayIcons[0];
         document.getElementById("game").innerHTML = "GoodBye!";
-
+        scoreUser = 0;
+        scorePc = 0;
+        document.getElementById("scoreGame").innerHTML = scoreUser + " - " + scorePc;
 }
 
 
